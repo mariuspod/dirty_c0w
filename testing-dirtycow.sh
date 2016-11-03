ca54cc4c61b8e0b9622ec410aa298edfebefc015
Marius Podwyszynski <mpodwyszynski@crealytics.de>
2016-11-03 13:13:30 +0100

    Make posting to librato conditional by ENV variable.

--- testing-dirtycow.sh
+++ testing-dirtycow.sh
@@ -8,4 +8,9 @@ chmod 404 foo
 ./dirtyc0w foo "$(/usr/games/cowsay 'MOOOOOOOOOOOOOOOO!, You are VULNERABLE! Update your kernel!')" &
 sleep 2
 cat foo
-/usr/bin/ruby post_status.rb
+if [ "$POST_TO_LIBRATO" = "true" ]
+then
+  echo "posting to librato:"
+  echo "https://metrics.librato.com/metrics/ih.dirty_c0w.vulnerable"
+  /usr/bin/ruby post_status.rb
+fi
